aec2d388802bad0cc5be4d653eb9487a9106ad55
coolscripter <seepana.naresh@gmail.com>
2018-05-23 17:01:24 +0000

    add http get

--- program.js
+++ program.js
@@ -1,53 +1,43 @@
-
-/**
- * ---------------------------------FILTERED LS ---------------------------------*/
-
- 
  var pathdir = process.argv[2];
- var fileExt = process.argv[3];
  
-//  console.log("dir path: "+pathdir);
-//  console.log("file ext: "+fileExt);
-var mymodule = require('./mymodule');
-mymodule(pathdir, fileExt, doread);
-function doread(err, items){
-      if(err){
-         return console.log(err);
-     }
-     if(items){
-        //  console.log(items);
-         items.forEach((filename, index, arr)=>{
-            //   console.log('-- found: ',filename);
-              
-                  console.log(filename);
-              
-         })
-        
-     }
- }
+//  console.log(pathdir);
+ var http = require("http");
  
-/**
- * ---------------------------------MY FIRST ASYNC I/O---------------------------------*/
-// let file = process.argv[2] || 'package.json';
-// var fs = require('fs');
-// fs.readFile(file, function doread(err, data){
-//     if(err){
-//         return console.log(err);
-//     }
-//     var lines = data.toString().split('\n').length - 1;
-//     console.log(lines)
-// });
-
-// /** ---------------------------------MY FIRST I/O--------------------------------- */
-// var fs = require('fs')
-//     var contents = fs.readFileSync(process.argv[2])
-//     var lines = contents.toString().split('\n').length - 1
-//     console.log(lines)
-
-// /** ---------------------------------My baby steps--------------------------------- */
-// var result = 0
-// for (var i = 2; i < process.argv.length; i++) {
-//   result += Number(process.argv[i])
-// }
-// console.log(result)
-// ------------------------------------------------------------------
+ http.get(pathdir, (res)=>{
+     const {statusCode} = res;
+     const contentType = res.headers['content-type'];
+    //  console.log('content-type: '+contentType);
+    //  console.log(res);
+    let error;
+    
+    if(statusCode != 200){
+        error = new Error('Request failed: /n'+ `Status code: ${statusCode}`);
+    }
+    // else if(!/^application\/json/.test(contentType)){
+    //     error = new Error('Invalid content type: '+ `expected application/json but received ${contentType}`);
+    // }
+    
+    if(error){
+        console.error(error.message);
+        res.resume();
+        return;
+    }
+    res.setEncoding('utf8');
+    
+    let responseData = '';
+    res.on('data', (chunk)=>{
+        // responseData += chunk;
+         console.log(chunk);
+    })
+    
+    //  res.on('data', console.log)
+     
+    res.on('end', ()=>{
+        // console.log(responseData);
+    })
+    
+    res.on('error', console.error);
+ }).on('error', (e)=>{
+     console.error(`Got error: ${e.message}`);
+ })
+ 
