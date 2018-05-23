80bef2b3f6745cb153c0a1e89fe7ee74f6f2ac5b
coolscripter <seepana.naresh@gmail.com>
2018-05-23 17:35:59 +0000

    add http get response bufferlist

--- program.js
+++ program.js
@@ -2,42 +2,63 @@
  
 //  console.log(pathdir);
  var http = require("http");
+ var BufferList = require("bl");
+  http.get(pathdir, (res)=>{
+        res.pipe(BufferList(function (err, content) {
+            if (err) {
+              return console.error(err)
+            }
+            content = content.toString()
+            console.log(content.length)
+            console.log(content);
+        }))
+  })
  
- http.get(pathdir, (res)=>{
-     const {statusCode} = res;
-     const contentType = res.headers['content-type'];
-    //  console.log('content-type: '+contentType);
-    //  console.log(res);
-    let error;
+//  http.get(pathdir, (res)=>{
+//      const {statusCode} = res;
+//      const contentType = res.headers['content-type'];
+//     //  console.log('content-type: '+contentType);
+//     //  console.log(res);
     
-    if(statusCode != 200){
-        error = new Error('Request failed: /n'+ `Status code: ${statusCode}`);
-    }
-    // else if(!/^application\/json/.test(contentType)){
-    //     error = new Error('Invalid content type: '+ `expected application/json but received ${contentType}`);
-    // }
     
-    if(error){
-        console.error(error.message);
-        res.resume();
-        return;
-    }
-    res.setEncoding('utf8');
+//     let error;
     
-    let responseData = '';
-    res.on('data', (chunk)=>{
-        // responseData += chunk;
-         console.log(chunk);
-    })
+//     if(statusCode != 200){
+//         error = new Error('Request failed: /n'+ `Status code: ${statusCode}`);
+//     }
+//     // else if(!/^application\/json/.test(contentType)){
+//     //     error = new Error('Invalid content type: '+ `expected application/json but received ${contentType}`);
+//     // }
     
-    //  res.on('data', console.log)
-     
-    res.on('end', ()=>{
-        // console.log(responseData);
-    })
+//     if(error){
+//         console.error(error.message);
+//         res.resume();
+//         return;
+//     }
     
-    res.on('error', console.error);
- }).on('error', (e)=>{
-     console.error(`Got error: ${e.message}`);
- })
+//     res.setEncoding('utf8');
+    
+//     let responseData = '';
+//     let bl = new BufferList();
+//     res.on('data', (chunk)=>{
+//         bl.append(new Buffer(chunk))
+//         responseData += chunk;
+//         //  console.log(chunk);
+      
+//     })
+    
+//     // res.pipe(BufferList(function(err, data)=>{
+//     //     console.log(data);
+//     // }))
+    
+//     res.on('end', ()=>{
+//         console.log(bl.length);
+//          console.log(responseData);
+//     })
+    
+//     res.on('error', console.error);
+    
+//  }).on('error', (e)=>{
+//      console.error(`Got error: ${e.message}`);
+//  })
  
