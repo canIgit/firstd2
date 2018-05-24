2a54bf1c9e391577ebc204beb9fa5bb4f17049f5
coolscripter <seepana.naresh@gmail.com>
2018-05-24 16:52:35 +0000

    http uppercaserer

--- program.js
+++ program.js
@@ -1,26 +1,27 @@
 var arg = process.argv[2];
 var http = require('http');
 var fs = require('fs');
+var map = require('through2-map');
 
 /**
  * TCP is a connection-oriented protocol meaning it first sets up a connection to the receiver then sends the data in segments (PDU for transport layer) which is carried by IP packets
  * http://codewinds.com/blog/2013-08-02-streams-basics.html
  */
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'content-type': 'text/plain' })
-  var filename = process.argv[3];
+http.createServer(function(instream, outstream) {
+  outstream.writeHead(200, { 'content-type': 'text/plain' })
+     if(instream.method !== 'POST'){
+         return instream.end('send me a POST\n')
+     }
+     
+    instream.pipe(map((chunk)=>{
+        // console.log(chunk.toString()+':--');
+        return chunk.toString().split('').join('-');
+    })).pipe(outstream);
 
-  // This line opens the file as a readable stream
-  var readStream = fs.createReadStream(filename);
 
-  // This will wait until we know the readable stream is actually valid before piping
-  readStream.on('open', function () {
-    // This just pipes the read stream to the response object
-    readStream.pipe(res);
-  });
-
-  // This catches any errors that happen while creating the readable stream
-  readStream.on('error', function(err) {
-    res.end(err);
-  });
+      // This catches any errors that happen while creating the readable stream
+      instream.on('error', function(err) {
+        outstream.end(err);
+      });
+ 
 }).listen(Number(arg));
